743791abd55fd8562d5bbaa8ba5cd8d5a0b8c13c
Tsvetan Tsvetkov <tsvetan.tsvetkov@efellows.bg>
2025-07-25 12:55:44 +0300

    add debugger

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -15,7 +15,7 @@ local function lsp_keymaps(bufnr)
   map("n", "gr", vim.lsp.buf.references, { desc = "Find references", unpack(opts) })
   map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation", unpack(opts) })
   map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation", unpack(opts) })
-  map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol", unpack(opts) })
+  map("n", "<leader>gn", vim.lsp.buf.rename, { desc = "Rename symbol", unpack(opts) })
   map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action", unpack(opts) })
   map("n", "<leader>dL", "<cmd>Telescope diagnostics<CR>", { desc = "Workspace Diagnostics" })
 end
